7b84cd037c6e5728e90021e8d544b92cdf23e19a
Jatin Kathrotiya <jatin.k@simformsolutions.com>
2023-09-22 13:38:43 +0530

    Migrate Swift Package to Binary Framework

--- NetworkEngine/Network/NetworkTask.swift
+++ NetworkEngine/Network/NetworkTask.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Alamofire
 
 /// Represents an HTTP task.
 public enum NetworkTask {
---
+++ NetworkEngine/NetworkEngine.h
@@ -0,0 +1,18 @@
+//
+//  NetworkEngine.h
+//  NetworkEngine
+//
+//  Created by Jatin Kathrotiya on 14/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkEngine.
+FOUNDATION_EXPORT double NetworkEngineVersionNumber;
+
+//! Project version string for NetworkEngine.
+FOUNDATION_EXPORT const unsigned char NetworkEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkEngine/PublicHeader.h>
+
+

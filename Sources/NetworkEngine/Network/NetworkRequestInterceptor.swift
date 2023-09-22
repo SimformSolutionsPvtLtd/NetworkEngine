497fed878b80ad344646377d2871e9ac6e827830
Jatin Kathrotiya <jatin.k@simformsolutions.com>
2023-09-22 14:36:17 +0530

    Remove SPM Files

--- Sources/NetworkEngine/Network/NetworkRequestInterceptor.swift
+++
@@ -1,9 +0,0 @@
-
-import Foundation
-import Alamofire
-
-/// A network request interceptor with adapt and retry methods
-/// - adapt: Configure the HTTP request before the call
-/// - retry: Define the retry policy
-public protocol NetworkRequestInterceptor: RequestInterceptor {
-}
